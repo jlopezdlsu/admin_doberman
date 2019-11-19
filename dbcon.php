31cd42bb7336bb2fae1652e65b8a6dee53c3eda0
phalange865 <phalange865@gmail.com>
2019-11-19 22:39:44 +0800

    changes

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,6 @@
 <?php
-$db_username        = 'root'; //MySql database username
-$db_password        = ''; //MySql database password
-$db_name            = 'db_doberman2'; //MySql database name
-$db_host            = 'localhost'; //MySql hostname or IP
+$db_username        = "dobermandb"; //MySql database username
+$db_password        = "wKue77tk0ovftrik"; //MySql database password
+$db_name            = "db_doberman"; //MySql database name
+$db_host            = "35.240.223.49:3306"; //MySql hostname or IP
  ?>
